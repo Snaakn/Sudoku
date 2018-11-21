27b84dc834a6f233f59ea9b2702efeafe201af08
Snakn <jan-philipp.krause@hotmail.de>
2018-11-21 11:54:56 +0100

    Working input validity check
    placeholder victory check
    gameloop

--- sudoku.c
+++ sudoku.c
@@ -2,8 +2,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 uint8_t field[9][9];
+char lastInput[] = {'0','0','0'};
 
 void draw() {
   printf("  __          _     _             \n");
@@ -32,33 +34,70 @@ void draw() {
   printf("  +−−−−−−−−−+−−−−−−−−−+−−−−−−−−−+\n");
 }
 
-void input(){
-  bool validInput[3] = {false,false,false};
-
-  do {
-    draw();
-    printf("Eingabe: ");
-    char input[6] = "      ";
-    const char allowed[] = {'1','2', '3','4','5','6','7','8','9','0'};
-    // write input to input via pointer
-    scanf("%5s,", &input[0]);
-
-    // list of valid characters for input
-    validInput[0] = validInput[1] = validInput[2] = false;
-    for (uint8_t i=0; i<=9; i++) {
-      if (input[0] == allowed[i]) {
-        printf("1 true\n");
-        validInput[0] = true;}
-      if (input[2] == allowed[i]) {
-        printf("2 true\n");
-        validInput[1] = true;}
-      if (input[4] == allowed[i]) {
-        printf("3 true\n");
-        validInput[2] = true;}
-    }
-  } while (validInput[0] == false || validInput[1] == false || validInput[2] == false);
+void check()
+{
+  for (uint8_t i = 0; i < 100; i++) {
+    if (i%5 == 0)
+      printf("*");
+  }
+  printf("\nchecked\n");
 }
 
+void game(){
+  // validity state of the input array
+  bool validInput[3] = {false,false,false};
+  bool isWon = false;
+
+  char input[6] = "------";
+  // game loop
+  while(!isWon){
+    draw();
+    do {
+      draw();
+      printf("Eingabe: ");
+      // fill input with spaces to avoid erros
+      //input = "      ";
+      // allowed characters for input
+      const char allowed[] = {'1','2', '3','4','5','6','7','8','9','0'};
+      // write input to input via pointer
+      scanf("%5s,", &input[0]);
+      printf("%c%c%c\n",input[0],input[2],input[4]);
+
+
+      validInput[0] = validInput[1] = validInput[2] = false;
+
+      for (uint8_t i=0; i<=9; i++) {
+        if (input[0] == allowed[i]) {
+          printf("1 true\n");
+          validInput[0] = true;}
+        if (input[2] == allowed[i]) {
+          printf("2 true\n");
+          validInput[1] = true;}
+        if (input[4] == allowed[i]) {
+          printf("3 true\n");
+          validInput[2] = true;}
+          if ((input[0]=='0' || input[2]=='0') && (input[0]!='0' || input[2]!='0' || input[4]!='0'))
+            validInput[0] = validInput[1] = validInput[2] = false;
+
+        if (input[0]=='c' && input[2]=='-' && input[4]=='-'){
+          check();
+          i = 10; //end for loop
+        };
+      }
+      // if (input[0]=='0' && input[2]=='0' && input[4]=='0') {
+      //   undo();
+      // }
+
+    } while (validInput[0] == false || validInput[1] == false || validInput[2] == false);
+    // saving last value of input coordinates for undo function
+    // idea: timeline for multiple undos
+    lastInput[0] = input[0];
+    lastInput[1] = input[2];
+    lastInput[2] = field[input[0]-1][input[2]-1];
+    field[atoi(&input[2])-1][atoi(&input[0])-1] = atoi(&input[4]);
+  }
+
+}// end of game loop
 
 int main() {
 // fill field with zeroes to avoid errors
@@ -67,7 +106,8 @@ int main() {
       field[x][y] = 0;
     }
   }
-  input();
+  game();
+  printf("GEWONNEN!\nGut gemacht!\n");
 
 
   return 0;
